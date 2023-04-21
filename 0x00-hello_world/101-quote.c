0e90b029572e49394f7de9e41c6b7436a6f88ad0
Mitchy5858 <mitchellejepkoech57@gmail.com>
2023-04-21 10:21:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-/** * main - Entry point
- * Return: Always 0 (success)
+/**
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
-}	
+}
