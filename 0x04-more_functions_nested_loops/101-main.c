211e9c279115e5144e0aa6025eb886e25a4d2432
Mitchy5858 <mitchellejepkoech57@gmail.com>
2023-04-27 23:39:06 +0000

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,18 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
-#endif
-
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    print_number(98);
+	        _putchar('\n');
+		    print_number(402);
+		        _putchar('\n');
+			    print_number(1024);
+			        _putchar('\n');
+				    print_number(0);
+				        _putchar('\n');
+					    print_number(-98);
+					        _putchar('\n');
+						    return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,19 @@
+
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
 #endif
 
