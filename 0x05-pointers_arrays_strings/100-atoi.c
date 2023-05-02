583a5f0b58c9b6271319ef6c1d2fa3981ab410e1
Mitchy5858 <mitchellejepkoech57@gmail.com>
2023-05-02 21:54:22 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,39 @@
-#include <stdio.h>
-#include <math.h>
+#include "main.h"
 /**
- * main - finds and prints the largest prime factor of the number 61285247514
- * followed by a new line
- * Return: Always 0 (Success)
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
-int main(void)
+int _atoi(char *s)
 {
-long int n;
-long int max;
-long int i;
-n = 612852475143;
-max = -1;
-while (n % 2 == 0)
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
 {
-max = 2;
-n /= 2;
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
 }
-for (i = 3; i <= sqrt(n); i = i + 2)
-{
-while (n % i == 0)
-{
-max = i;
-n = n / i;
+i++;
 }
-}
-if (n > 2)
-max = n;
-printf("%ld\n", max);
+if (f == 0)
 return (0);
+return (n);
 }
