7abca20567dcd523b78e9caa786642f32ed23609
Mitchy5858 <mitchellejepkoech57@gmail.com>
2023-05-04 22:30:28 +0000

    2-main.c

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    printf("%d\n", mul(98, 1024));
-	        printf("%d\n", mul(-402, 4096));
-		    return (0);
+printf("%d\n", mul(98, 1024));
+printf("%d\n", mul(-402, 4096));
+return (0);
 }
 
