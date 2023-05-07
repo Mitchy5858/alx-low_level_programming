9c457e4524fd73238df21f7d032377b1bd8a0f37
Mitchy5858 <mitchellejepkoech57@gmail.com>
2023-05-07 19:39:39 +0000

    3-print_remaining_days.c

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -18,9 +18,9 @@ year = 1997;
 
 printf("Date: %02d/%02d/%04d\n", month,  day,  year);
 
-day = convert_day(month,day);
+day = int convert_day(month, day);
 
-print_remaining_days(month,day,year);
+void print_remaining_days(month, day, year);
 
 return (0);
 }
