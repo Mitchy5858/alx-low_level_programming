2800c5945bb07e32b3218095529f459c6e382479
Mitchy5858 <mitchellejepkoech57@gmail.com>
2023-05-07 20:43:54 +0000

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,27 +9,39 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
+int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int day_count = 0;
+int i;
+for (i = 1; i < month; i++)
 {
-if (month >= 2 && day >= 60)
-{
-day++;
+day_count += days_in_month[i];
 }
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+day_count += day;
+
+if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+{
+days_in_month[2] = 29;
+}
+
+if (month > 2 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)))
+{
+day_count++;
+}
+
+if (month < 1 || month > 12)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+}
+else if (day < 1 || day > days_in_month[month])
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 }
 else
 {
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
+printf("Day of the year: %d\n", day_count);
+printf("Remaining days: %d\n", 365 + ((year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 1 : 0) - day_count);
 }
 }
