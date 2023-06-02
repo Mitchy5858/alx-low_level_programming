93e360aa3ecf2705461b421c5c8762675f810b94
Mitchy5858 <mitchellejepkoech57@gmail.com>
2023-06-02 20:38:10 +0000

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    format db 'Hello, Holberton', 0
-
-section .text
-    extern printf
-
-global main
+global    main
+          extern    printf
 main:
-    sub rsp, 8              ; Align the stack
-
-    mov rdi, format         ; Set the format string
-    xor rax, rax            ; Clear rax
-    call printf             ; Call printf
-
-    add rsp, 8              ; Restore the stack
-    xor eax, eax            ; Clear eax
-    ret                     ; Return from main
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
